973ace9b770a5c3a80ce875329a9c02324d87b64
Shrayash awasthi <awasthishrayashofc@gmail.com>
2025-06-28 21:58:46 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (26.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // vector<int> s_count(256,0);
-        // vector<int> t_count(256,0);
-        // for(int i=0;i<s.size();i++){
-        //     s_count[s[i]-'0']++;
+        if (s.size() != t.size()) return false;
+        vector<int> freq(26, 0);
+        // find the frequency of each character from the s string
+        for (char c : s) freq[c - 'a']++;
+        // decrease the frequency of each character from the string t , if overall frequency is zero then strings are anagaram
+        for (char c : t) freq[c - 'a']--;
+        for (int count : freq) if (count != 0) return false;
+        return true;
+
+        // second method with nlogn time complexity
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s==t){
+        //     return true;
         // }
-        // for(int i=0;i<s.size();i++){
-        //     t_count[t[i]-'0']++;
-        // }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
-        }
-        return false;
+        // return false;
 
     }
 };
